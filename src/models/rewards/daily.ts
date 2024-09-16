4b60174059777a146dba0a33eda305a68e4d34a2
Yuriy <berlandspw@gmail.com>
2024-09-16 16:54:57 +0500

    fix

--- src/models/rewards/daily.ts
+++ src/models/rewards/daily.ts
@@ -10,9 +10,10 @@ export async function addDailyRewardNote (userId: number): Promise<number> {
    const dateSec = Math.round(new Date().getTime() / 1000);
    const query = `
    INSERT INTO "daily_rewards" (user_id, last_received)
-   VALUES (${userId}, ${dateSec}) ON CONFLICT (user_id) DO
-   UPDATE "daily_rewards" SET last_received = ${dateSec} WHERE
-   user_id = ${userId} RETURNING last_received;`;
+   VALUES (${userId}, ${dateSec})
+   ON CONFLICT (user_id) DO UPDATE
+   SET last_received = EXCLUDED.last_received
+   RETURNING last_received;`;
    const result = await runQuery(query, true);
    return result && result.length > 0 ? result[0].last_received || 0 : 0
 }
