ecd9d6fb4b58e4caca324d9c6dc5c28be3caf782
Yuriy <berlandspw@gmail.com>
2024-09-19 17:24:40 +0500

    stat update

--- src/models/stats/referral.ts
+++ src/models/stats/referral.ts
@@ -1,3 +1,4 @@
+import { dateSec } from "utils/text";
 import { runQuery } from "../../models/connection"
 
 export async function getReferralStats (): Promise<{inviter_id: number, user_count: number}[]> {
@@ -10,11 +11,32 @@ export async function getReferralStats (): Promise<{inviter_id: number, user_cou
     return result || []
 }
 
-export async function geFinishedDuelsCount (): Promise<number> {
+export async function getFinishedDuelsCount (): Promise<number> {
     const query = `SELECT count(*) FROM duels WHERE user_2_id IS NOT NULL;`;
     const result = await runQuery(query, true);
     return result && result.length > 0 ? result[0].count : 0
 }
+
+export async function getAllDuelsCount (): Promise<number> {
+    const query = `SELECT count(*) FROM duels;`;
+    const result = await runQuery(query, true);
+    return result && result.length > 0 ? result[0].count : 0
+}
+
+export async function getDuelsCountLastDate (): Promise<number> {
+    const date = dateSec() - 86400;
+    const query = `SELECT count(*) FROM duels WHERE creation >= ${date};`;
+    const result = await runQuery(query, true);
+    return result && result.length > 0 ? result[0].count : NaN
+}
+
+export async function getDailyRewardReceiversCount (): Promise<number> {
+    const date = dateSec() - 86400;
+    const query = `SELECT count(*) FROM daily_rewards WHERE last_received >= ${date};`;
+    const result = await runQuery(query, true);
+    return result && result.length > 0 ? result[0].count : NaN
+}
+
 
 export async function getTotalUsers (): Promise<number> {
    const query = `SELECT count(*) FROM "users";`;
