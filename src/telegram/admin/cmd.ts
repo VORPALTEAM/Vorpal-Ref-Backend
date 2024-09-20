ba680b15e5e550bfda542dd6f6a69501242caabd
Yuriy <berlandspw@gmail.com>
2024-09-20 18:39:43 +0500

    post testing

--- src/telegram/admin/cmd.ts
+++ src/telegram/admin/cmd.ts
@@ -9,12 +9,24 @@ import TelegramBot from 'node-telegram-bot-api';
 import { sendMessageWithSave } from '../handlers/utils';
 import { createDuelInTournamentAction } from './tournament';
 import { downloadReferralStats } from './referral';
+import { getAdminSession } from './session';
+
+export const adminCmdInfo = {
+      help: "Get list of all commands",
+      init_post: "Start to create post, next message will sent to all members",
+      create_tournament: "New tournament",
+      referral_stats: "Get bot statistics in csv file",
+      create_duel: "Create duel in tournament, nex 2 args will be a members' ids",
+      get_participants: "Get list of last tournament participants"
+}
 
 export const admin_commands = [
   'create_duel',
   'create_tournament',
   'get_participants',
-  'referral_stats'
+  'referral_stats',
+  'init_post',
+  'help'
 ];
 
 export const adminCmdHandler = async (
@@ -41,6 +53,12 @@ export const adminCmdHandler = async (
     sendMessageWithSave(bot, chat, `No rights to send admin commands`);
     return;
   }
+  const session = getAdminSession(String(chat));
+  if (session.getLastAction() === admin_commands[4]) {
+    sendMessageWithSave(bot, chat, `Will send it: ${cmd}`);
+    return;
+  }
+  session.setLastAction(command);
   switch (command) {
     case admin_commands[0]:
       if (cmdData.length < 4) {
@@ -88,6 +106,12 @@ export const adminCmdHandler = async (
       break;
     case admin_commands[3]: 
       return downloadReferralStats(bot, msg)
+    case admin_commands[3]: 
+      return sendMessageWithSave(
+        bot,
+        chat,
+        `Write your post. Warning! Next message will sent for all users!`,
+      );
     default:
       sendMessageWithSave(
         bot,
