613e74f7460bc3ad2eed739c972680dbe53b5a03
Yuriy <berlandspw@gmail.com>
2024-10-07 14:53:43 +0500

    tour admin fix

--- src/models/createDB/tables.ts
+++ src/models/createDB/tables.ts
@@ -220,11 +220,11 @@ const tableCreationQueries = [
         date_start integer,
         date_end integer
     );`,
-    `CREATE TABLE IF NOT EXISTS tournament_admins (
-        id SERIAL PRIMARY KEY,
-        tournament_id integer,
-        admin_id integer,
-        UNIQUE (tournament_id, admin_id)
+    `CREATE TABLE tournament_admins (
+     id SERIAL PRIMARY KEY,
+     tournament_id INTEGER,
+     admin_id INTEGER,
+     UNIQUE (tournament_id, admin_id)
     );`,
     `CREATE TABLE IF NOT EXISTS tournament_winners (
       id SERIAL PRIMARY KEY,
