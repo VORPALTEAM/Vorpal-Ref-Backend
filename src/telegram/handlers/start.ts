077376089c61e779a3a7b751839c8c1a447d1b5a
Yuriy <berlandspw@gmail.com>
2024-10-20 20:53:58 +0500

    fix & test

--- src/telegram/handlers/start.ts
+++ src/telegram/handlers/start.ts
@@ -34,6 +34,7 @@ export const startHandler = async (bot: TelegramBot, msg: TelegramBot.Message, m
     };
 
     const inviter = match[1]?.toLowerCase();
+    console.log("Conditions:", inviter, inviter.indexOf("ref"));
     const isNewRef = inviter && inviter.indexOf("ref") === 0;
     console.log("Is new ref:", isNewRef);
     // If this is a tournament registration
